d99b57c51c726cdc230147b1ed80d03dd49e9161
SilviaAfer <silviafernandez26@hotmail.com>
2026-04-24 15:13:47 -0500

    Lab resuelto

--- main.py
+++ main.py
@@ -1,5 +1,38 @@
 def main():
-  print("Hello learners!")
+  print("Welcome to my new and better calculator By Silvi!")
 
 if __name__=="__main__":
   main()
+
+#This is for the New Lab, Building a new and better calculator
+
+  #Addmultiplenumbers([num, num, ..])
+def addmultiplenumbers(numbers) :
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+#Multiplymultiplenumbers
+def multiplymultiplenumbers(numbers):
+    result = 1
+    for num in numbers:
+        result *= num
+    return result
+
+#Isiteven 
+def isiteven(num):
+    # Must be an integer AND divisible by 2
+    return isinstance(num, int) and num % 2 == 0
+
+#isitaninteger(num)
+def isitaninteger(num):
+    return isinstance(num, int)
+
+
+def main():
+    print("Bye bye!")
+
+
+if __name__ == "__main__":
+    main()
